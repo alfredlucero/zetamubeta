87f24c5450095b6c22c15a9e32c4f756e810f154
Alfred Lucero <alfred.lucero@ucla.edu>
2016-11-22 17:06:47 -0800

    changed fonts, trying out overlays on the home cornerstone banners, adding images, changed borders of panels and menu items; TODO: try and experiment with angularjs in having dynamic views change based on links

--- jquery/jquery-extended.js
+++
@@ -1,9 +0,0 @@
-// Extend jQuery to use animate.css easily by doing $('#your-element').animateCss('bounce');
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
