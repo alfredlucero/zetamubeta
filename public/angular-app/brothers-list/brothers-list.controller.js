dd2759cbe4cfa2cb9c6dc10b42618ca3d9c10e0c
Alfred Lucero <alfred.lucero@ucla.edu>
2016-12-20 01:46:52 -0800

    laid out the main page photos, outlined all the sections but need to add content, added contacts content

--- public/angular-app/brothers-list/brothers-list.controller.js
+++ public/angular-app/brothers-list/brothers-list.controller.js
@@ -3,7 +3,7 @@ angular.module('zmbApp').controller('BrothersController', BrothersController);
 
 function BrothersController(brotherDataFactory) {
 	var vm = this;
-
+	vm.filterBrothers = '';
 	brotherDataFactory.brothersList().then(function(response) {
 		console.log(response);
 		vm.brothers = response;
